9db9753040301b09a342cfccd27631b7ae5c5882
zilongshanren <guanghui8827@126.com>
2015-10-12 21:14:59 +0800

    add delete functions

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 /*global Editor */
-
+var fs = require('fs')
 var chokidar = require('chokidar');
+
+rmDir = function(dirPath) {
+    try { var files = fs.readdirSync(dirPath); }
+    catch(e) { return; }
+    if (files.length > 0)
+        for (var i = 0; i < files.length; i++) {
+            var filePath = dirPath + '/' + files[i];
+            if (fs.statSync(filePath).isFile())
+                fs.unlinkSync(filePath);
+            else
+                rmDir(filePath);
+        }
+    fs.rmdirSync(dirPath);
+};
 
 module.exports = {
     load: function() {
@@ -16,9 +30,12 @@ module.exports = {
             this._fileWather = null;
         }
     },
+    'file-browser:delete' : function(filepath){
+        rmDir(filepath);
+    },
     'file-browser:watch-file-change': function(filePath) {
         this._cleanUpWatcher();
-
+        
         this._fileWather = chokidar.watch(filePath, {
             persistent: true,
             followSymlinks: true,
@@ -30,27 +47,23 @@ module.exports = {
         this._fileWather
             .on('add', function(path) {
                 log('File', path, 'has been add');
-                this._refreshFileBrowser(path);
+                Editor.sendToPanel('file-browser.panel', 'file-browser:add-item', path , "file");
             }.bind(this))
             .on('change', function(path) {
                 log('File', path, 'has been changed');
-                this._refreshFileBrowser(path);
             }.bind(this))
             .on('unlink', function(path) {
                 log('File', path, 'has been removed');
-                this._refreshFileBrowser(path);
+                Editor.sendToPanel('file-browser.panel', 'file-browser:delete-item', path);
             }.bind(this))
             .on('addDir', function(path) {
                 log('Directory', path, 'has been added');
-                this._refreshFileBrowser(path);
+                Editor.sendToPanel('file-browser.panel', 'file-browser:add-item', path, "dir");
             }.bind(this))
             .on('unlinkDir', function(path) {
                 log('Directory', path, 'has been removed');
-                this._refreshFileBrowser(path);
+                Editor.sendToPanel('file-browser.panel', 'file-browser:delete-item', path);
             }.bind(this));
-    },
-    _refreshFileBrowser : function(path) {
-        Editor.sendToPanel('file-browser.panel', 'file-browser:add-item', path );
     },
 
     'file-browser:open': function() {
--- panel/panel.js
+++ panel/panel.js
@@ -1,88 +1,52 @@
 /*global Editor */
 
 (function() {
-    var fs = require('fs');
+    // var fs = require('fs');
 
     Editor.registerPanel('file-browser.panel', {
         is: 'file-browser',
         _treeRoot: null,
         _filePath: '',
-
-        listeners : {
-            'item-select': '_onItemSelect',
+        _currentSelection: null,
+        'file-browser:delete-item': function(path){
+            var item = document.getElementById(path);
+            item.parentNode.removeChild(item);
         },
+        'file-browser:add-item': function(path, type) {
+            var filename = path.slice(this._filePath.length);
+            var treeRoot = this.$.tree;
+            var child = document.createElement('p');
+            child.innerText = path;
+            child.id = path;
+            child.addEventListener('click', function() {
+                // TODO:
+                child.style.background = '#333333';
+                this._currentSelection = path;
+                this.$.status.innerText = filename + ' is selected';
+            }.bind(this), false);
+            treeRoot.appendChild(child);
 
-        _onItemSelect: function ( event ) {
-            event.stopPropagation();
-
-            if ( event.detail.shift ) {
-                Editor.Selection.confirm();
-            } else if ( event.detail.toggle ) {
-                Editor.Selection.confirm();
-            } else {
-                Editor.Selection.select( 'asset', event.target._userId, true );
+            if (type === 'file') {
+                this.$.status.innerText = 'file is added.';
+            } else if (type === 'dir') {
+                this.$.status.innerText = 'directory is added.';
             }
-        },
-        _generateFileTreeView: function(dir, rootNode) {
-            var files = fs.readdirSync(dir);
-            files.forEach(function(file) {
-                //add file to tree
-                var fileFullPath = dir + '/' + file;
-                var fileItem = this.newEntry();
-                this.$.tree.addItem(rootNode, fileItem, {
-                    id: fileFullPath,
-                    name: file
-                });
-                if (fs.statSync(fileFullPath).isDirectory()) {
-                    this._generateFileTreeView(fileFullPath, fileItem);
-                    fileItem.folded = false;
-                }
-            }.bind(this));
-        },
-        'file-browser:add-item': function(path) {
-            this._refreshOpen();
-        },
-        _refreshOpen: function() {
-            this.$.tree.clear();
-
-            this._treeRoot = this.newEntry();
-            this.$.tree.addItem(this.$.tree, this._treeRoot, {
-                id: 'tree' + this._filePath,
-                name: this._filePath
-            });
-
-            this._generateFileTreeView(this._filePath,
-                this._treeRoot,
-                function(error, files) {
-                    console.log(files);
-                });
-
-            this.$.tree.selectItemById('tree' + this._filePath);
-            this._treeRoot.folded = false;
-
-        },
-        'selection:selected': function ( type, ids ) {
-            if ( type !== 'asset' )
-            {
-                return;
-            }
-
-            ids.forEach( function ( id ) {
-                this.$.tree.selectItemById(id);
-            }.bind(this));
         },
         onDeleteKeyPressed: function() {
-
+            Editor.sendToCore('file-browser:delete', this._currentSelection);
+        },
+        selectPrev : function() {
+            this.$.status.innerText = 'select prev';
+        },
+        selectNext: function() {
+            this.$.status.innerText = 'select next';
         },
         _openPath: function() {
-            this._refreshOpen();
             //watch file change
             Editor.sendToCore('file-browser:watch-file-change', this._filePath);
         },
-        ready: function() {},
-        newEntry: function() {
-            var ctor = Editor.elements['tree-item'];
-            return new ctor();
+        ready: function() {
+            this.$.status.style.background = 'Green';
         }
     });
 })();
